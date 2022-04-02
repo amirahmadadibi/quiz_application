2beb92565dea2233f628da64cb65b7b4dddf014e
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-04-02 20:15:59 +0430

    week6 E13-desing quiz page and use list tile widget

--- lib/quiz_page.dart
+++ lib/quiz_page.dart
@@ -15,7 +15,53 @@ class QuizPage extends StatelessWidget {
         ),
       ),
       body: SafeArea(
-        child: Text('quizzz'),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: double.infinity,
+            ),
+            Image(
+              image: AssetImage('images/2.png'),
+              height: 300,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              'مشهورترین شعبده‌باز دنیا کیست؟‌',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            ListTile(
+              title: Text(
+                'پاسخ اول',
+                textAlign: TextAlign.end,
+              ),
+            ),
+            ListTile(
+              title: Text(
+                'پاسخ دوم',
+                textAlign: TextAlign.end,
+              ),
+            ),
+            ListTile(
+              title: Text(
+                'پاسخ سوم',
+                textAlign: TextAlign.end,
+              ),
+            ),
+            ListTile(
+              title: Text(
+                'پاسخ چهارم',
+                textAlign: TextAlign.end,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
