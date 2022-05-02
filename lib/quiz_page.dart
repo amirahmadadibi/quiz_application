35f9262c5c7a186250ac8d58c001c1e18b4be552
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-05-02 23:25:10 +0430

    week8 E08 check correct and wrong answer

--- lib/quiz_page.dart
+++ lib/quiz_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quizz_like_application/constants/Constants.dart';
+import 'package:quizz_like_application/data/Question.dart';
 
 class QuizPage extends StatefulWidget {
   const QuizPage({Key? key}) : super(key: key);
@@ -10,34 +11,11 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   int shownQuestionIndex = 0;
-  var list = [
-    ListTile(
-        title: Text(
-      'پاسخ اول',
-      textAlign: TextAlign.end,
-    )),
-    ListTile(
-      title: Text(
-        'پاسخ دوم',
-        textAlign: TextAlign.end,
-      ),
-    ),
-    ListTile(
-      title: Text(
-        'پاسخ سوم',
-        textAlign: TextAlign.end,
-      ),
-    ),
-    ListTile(
-      title: Text(
-        'پاسخ چهارم',
-        textAlign: TextAlign.end,
-      ),
-    ),
-  ];
+  Qustion? selectedQuestion;
 
   @override
   Widget build(BuildContext context) {
+    selectedQuestion = getQuestionsList()[shownQuestionIndex];
     String questionImageIndex =
         getQuestionsList()[shownQuestionIndex].imageNameNubmer!;
     return Scaffold(
@@ -64,25 +42,33 @@ class _QuizPageState extends State<QuizPage> {
               height: 30,
             ),
             Text(
-              getQuestionsList()[shownQuestionIndex].questionTitle!,
+              selectedQuestion!.questionTitle!,
               textAlign: TextAlign.center,
               style: TextStyle(fontSize: 18),
             ),
             SizedBox(
               height: 30,
             ),
-            ...List.generate(
-              4,
-              (index) => ListTile(
-                title: Text(
-                  getQuestionsList()[shownQuestionIndex].answerList![index],
-                  textAlign: TextAlign.end,
-                ),
-              ),
-            )
+            ...List.generate(4, (index) => getOptionsItem(index))
           ],
         ),
       ),
+    );
+  }
+
+  Widget getOptionsItem(int index) {
+    return ListTile(
+      title: Text(
+        selectedQuestion!.answerList![index],
+        textAlign: TextAlign.end,
+      ),
+      onTap: () {
+        if (selectedQuestion!.correctAnswer == index) {
+          print('correct');
+        } else {
+          print('wrong');
+        }
+      },
     );
   }
 }
